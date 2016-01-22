e713b09f622c8e9060940b0b04c469071c8d6860
lmhonorio <lmhonorio@gmail.com>
2016-01-22 13:51:44 -0200

    nova2

--- pSGS/pSGS/Projections.py
+++ pSGS/pSGS/Projections.py
@@ -13,16 +13,56 @@ class clsProjectionOverAxis(object):
 		self.volume = 1.0
 		self.box_ref_center = []
 		self.dimension = data.shape[1]
+		self.reference = world_ref_center
+		self.updateAxesAndProjections(data)
+
+		#self.cov = np.cov(data.transpose())
+		#w, v = np.linalg.eig(self.cov)
+		#self.pi = v
+		#self.Lambda = w
+		#self.mod_pi = [np.linalg.norm(pi) for pi in self.pi]
+
+		#self.__iextern = []
+
+
+
+		#for i in range(0,self.dimension):
+		#	projection_over_pi = np.array(np.dot(self.data_to_world_ref_center, self.pi[i,:]) / self.mod_pi[i])
+		#	vmin = projection_over_pi.min(0)
+		#	vmax = projection_over_pi.max(0)
+		#	self.__iextern.append(projection_over_pi.argmin(0))
+		#	self.__iextern.append(projection_over_pi.argmax(0))
+		#	self.projection_over_pi.append(projection_over_pi)
+		#	self.min.append(vmin)
+		#	self.max.append(vmax)
+		#	delta_lambda = (np.array(vmax) - np.array(vmin))/2.0
+		#	self.delta_lambda.append(delta_lambda)
+		#	self.volume *= self.volume * delta_lambda 
+
+		##self.center = center + 0.5 * np.dot(np.array(self.min) + np.array(self.max),axis)
+		##o min e max se referem ao centro na coordenada do box, para a coordenada do mundo devemos transformar
+		## de acordo com o centro do mundo e a matriz de rotação dada pelos autovetores.
+		#self.box_ref_center = world_ref_center + 0.5 * np.dot(self.pi.transpose(),np.array(self.min) + np.array(self.max))
+
+
+
+	def updateAxesAndProjections(self,data, world_ref_center):
 		self.cov = np.cov(data.transpose())
 		w, v = np.linalg.eig(self.cov)
 		self.pi = v
 		self.Lambda = w
-		self.mod_pi = [np.linalg.norm(pi) for pi in self.cov]
+		self.mod_pi = [np.linalg.norm(pi) for pi in self.pi]
+
+		self.__iextern = []
+
+
 
 		for i in range(0,self.dimension):
 			projection_over_pi = np.array(np.dot(self.data_to_world_ref_center, self.pi[i,:]) / self.mod_pi[i])
 			vmin = projection_over_pi.min(0)
 			vmax = projection_over_pi.max(0)
+			self.__iextern.append(projection_over_pi.argmin(0))
+			self.__iextern.append(projection_over_pi.argmax(0))
 			self.projection_over_pi.append(projection_over_pi)
 			self.min.append(vmin)
 			self.max.append(vmax)
@@ -37,8 +77,19 @@ class clsProjectionOverAxis(object):
 
 
 	def improveDataFitness(self):
-		i = 1
-		pass
+		ext_points = []
+		ext_points.append(self.data_to_world_ref_center[self.__iextern,:])
+		ext_array = np.array(ext_points)
+		self.updateAxesAndProjections(ext_array, self.reference)
+
+
+
+		#self.cov = np.cov(ext_array.transpose())
+		#w, v = np.linalg.eig(self.cov)
+		#self.pi = v
+		#self.Lambda = w
+		#self.mod_pi = [np.linalg.norm(pi) for pi in self.pi]
+
 	
 
 	def myAxis(self, i:int):
