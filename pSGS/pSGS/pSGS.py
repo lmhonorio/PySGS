a8d7045a1924ecf6979cdbc7d3ecbc0f8f524844
lmhonorio <lmhonorio@gmail.com>
2016-02-24 15:10:33 -0300

    c2

--- pSGS/pSGS/pSGS.py
+++ pSGS/pSGS/pSGS.py
@@ -11,12 +11,12 @@ import PrintData
 
 
 
-t0 = time.clock()
+t0 = time.time()
 
 myData = DataManagement.clsDataIO()
 
 #list_cls = myData.returnBananaDataset((100,100), (2.0,1.0), 1.7, 0.2)
-list_cls = myData.returnCheckerBoardFunction(50,2,1,0.55)
+list_cls = myData.returnCheckerBoardFunction(50,2,1,0.3)
 
 obhbs = OrientedBoundHyperBox.clsListofOBHB(list_cls)
 
@@ -24,13 +24,13 @@ has_collision, separating_axes, MtrCol = SeparatingAxes.clsCollisionTest.TestAll
 
 print('colisao: ' + str(bool(has_collision)) + ' , ' + str(len(separating_axes.separatin_axes)))
 							 
-print(time.clock() - t0)
+print(time.time() - t0)
 print(list_cls[0].x_mean)
 print(list_cls[0].word_ref_center)
 
 
 
-print(time.clock() - t0)
+print(time.time() - t0)
 PrintData.clsPrint.print2dmodel(obhbs)
 i=1
 
